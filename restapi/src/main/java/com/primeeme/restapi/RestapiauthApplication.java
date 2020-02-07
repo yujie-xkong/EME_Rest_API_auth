f8b6676a8d77360227318346712ced36e0804f64
xkong001 <xianghe_kong@yahoo.com>
2020-02-06 21:28:18 -0700

    add springboot base

---
+++ restapi/src/main/java/com/primeeme/restapi/RestapiauthApplication.java
@@ -0,0 +1,13 @@
+package com.primeeme.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiauthApplication.class, args);
+	}
+
+}
---
+++ restapi/src/test/java/com/primeeme/restapi/RestapiauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeeme.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
